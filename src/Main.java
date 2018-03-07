12df0503871829a4fdf2d1bc758bb5ebda1c510b
wager <jiaqing.wang@weimob.com>
2018-03-07 11:41:42 +0800

    v2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int sum  = 0;
+        int a = 1;
+        int b = 2;
+        sum = a+b;
+        System.out.println(sum);
     }
 }
